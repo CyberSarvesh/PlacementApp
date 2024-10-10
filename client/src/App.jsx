12c1400d70fd75633bb57f9fba1a9e535bf36bb8
Sarvesh Mohite <142711109+CyberSarvesh@users.noreply.github.com>
2024-10-10 22:08:58 +0530

    this is new modified basic frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,19 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import AdminPage from './pages/AdminPage';
-import StudentPage from './pages/StudentPage';
-import HRPage from './pages/HRPage';
-import LoginPage from './pages/LoginPage';
-import { AuthContext } from './context/AuthContext';  // Context to manage roles
+// src/components/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import StudentDashboard from './StudentDashboard';
+import HRDashboard from './HRDashboard';
 
 const App = () => {
-  const { user } = useContext(AuthContext);
-
   return (
-    <>
-		<Router>
-			<Routes>
-				{/* Public Routes */}
-				<Route path="/login" element={<LoginPage />} />
-
-				{/* Protected Routes for students dashboard and everything*/}
-				<Route
-				path="/student/*"
-				element={user?.role === 'student' ? <StudentPage /> : <Navigate to="/login" />}
-				/>
-
-				{/* Protected Routes for admin dashboard and everything*/}
-				<Route
-				path="/admin/*"
-				element={user?.role !== 'admin' ? <AdminPage /> : <Navigate to="/login" />}
-				/>
-
-				{/* Protected Routes for hr dashboard and everything*/}
-				<Route
-				path="/hr/*"
-				element={user?.role === 'hr' ? <HRPage /> : <Navigate to="/login" />}
-				/>
-
-				{/* Default Redirect */}
-				<Route path="*" element={<Navigate to="/login" />} />
-			</Routes>
-		</Router>
-    </>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/hr-dashboard" element={<HRDashboard />} />
+      </Routes>
+    </Router>
   );
 };
 
--- client/src/components/Shared/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export default Footer = () => {
-  return (
-    <div>This is the footer of the page containing all the copyrights and licenses</div>
-  )
-}
